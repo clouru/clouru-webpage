348beeed1dbe706a7d54d70874265d5e9131ffb9
unknown <songlin@eagoats.com>
2021-05-06 16:09:25 -0700

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,52 @@
+// $(document).ready(function () {
+//   $(".logos-slider ul").slick({
+//     dots: false,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 8,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     responsive: [
+//       {
+//         breakpoint: 1200,
+//         settings: {
+//           slidesToShow: 6,
+//           slidesToScroll: 1
+//         }
+//       },
+//       {
+//         breakpoint: 990,
+//         settings: {
+//           slidesToShow: 4,
+//           slidesToScroll: 1
+//         }
+//       },
+//       {
+//         breakpoint: 767,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 1
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 1
+//         }
+//       }
+//     ]
+//   });
+//   $(".slide-wrap").slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     autoplay: true,
+//     autoplaySpeed: 2000
+//   });
+// });
