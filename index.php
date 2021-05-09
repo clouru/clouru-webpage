248cdb3e70cc4f0eee0f633b0b85003de2ff70e2
IvanVoda <toptalentdev@gmail.com>
2021-05-09 15:24:03 -0700

    update main.html

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-	$to_email = 'toptalentdev@gmail.com';
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/main.html');
+
+  $to_email = 'toptalentdev@gmail.com';
 	$subject = 'Testing PHP Mail';
 	$message = 'This mail is sent using the PHP mail function';
 	$headers = 'From: test@test.com';
